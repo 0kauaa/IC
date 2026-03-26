b87e5795f9dc2c5f3def829aa6c825332f495b04
Kauã Santana <kauassilva0023@gmail.com>
2026-03-26 19:11:02 -0300

    otimiza implementação dos morfismos do regressor.

--- scripts/models/LinearRegressor.hs
+++ scripts/models/LinearRegressor.hs
@@ -3,7 +3,7 @@ module LinearRegressor where
 import Learner
 import Params
 
--- learner regressor = r(p, x, y), onde p = {w, b}
+-- learner regressor linear = r([p], x, y), onde [p] = [{w, b}]
 regressor :: Learner '[Double, Double] Double Double -- Learner [w, b] x y
 regressor = Learner
     {
@@ -14,23 +14,28 @@ regressor = Learner
         -- dado um erro, atualiza os parâmetros
         u = \(w ::: b ::: ParamsNull) x y ->
 
+                -- predição
+            let ŷ  = (x * w + b)
                 -- erro da prdição
-            let e  = (x * w + b) - y          -- yhat - y
+                e  = ŷ - y                 -- yhat - y
                 -- novo w
                 w' = w - ep * e * x           -- gradiente em w
                 -- novo b
                 b' = b - ep * e               -- gradiente em b
                 
                 -- novo espaço de parâmetros
-                in w' ::: b' ::: Params Null,
+                in w' ::: b' ::: ParamsNull,
 
         -- dado um erro, atualiza a entrada
         r = \(w ::: b ::: ParamsNull) x y ->
             
+                -- predição
+            let ŷ = (x * w + b)
                 -- erro da predição
-            let e = (x * w + b) - y
+                e = ŷ - y
                 
-                in a - ep * e * w,
+                -- novas entradas
+                in x - ep * e * w,
         
         iniParam = 0.0 ::: 0.0 ::: ParamsNull
 
