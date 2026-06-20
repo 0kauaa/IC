57c4973574e9e35c2f41fe32caedd03a3d1e19c7
Kauã Santana <kauassilva0023@gmail.com>
2026-06-20 14:09:08 -0300

    adicona mais um componente ao pca do iris.

--- tools/python/pca_iris.py
+++ tools/python/pca_iris.py
@@ -1,13 +1,13 @@
-# aplica o pca, com 1 componente, ao iris e salva em iniciacao-cientifica/data/iris_pca.csv
+# aplica o pca, com 2 componentes, ao iris e salva em iniciacao-cientifica/data/iris_pca.csv
 from pandas import read_csv, DataFrame
 from sklearn.decomposition import PCA 
 
 df = read_csv("../../data/iris.csv", index_col=False)
 x  = df.select_dtypes(exclude='object')
 
-pca   = PCA(n_components=1)
+pca   = PCA(n_components=2)
 x_pca = DataFrame(pca.fit_transform(X=x))
-print(pca.explained_variance_ratio_) # [0.92461872]
+print(pca.explained_variance_ratio_) # [0.92461872 0.05306648]
 
-x_pca.columns = ['pca']
+x_pca.columns = ['pca1', "pca2"]
 x_pca.to_csv("../../data/iris_pca.csv", index=False)
