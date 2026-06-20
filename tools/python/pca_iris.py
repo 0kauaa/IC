46b43e6e63495d846fde9f3009455f6149e2fd1d
Kauã Santana <kauassilva0023@gmail.com>
2026-06-20 14:03:05 -0300

    renomeia a coluna do iris_pca.

--- tools/python/pca_iris.py
+++ tools/python/pca_iris.py
@@ -2,11 +2,12 @@
 from pandas import read_csv, DataFrame
 from sklearn.decomposition import PCA 
 
-df = read_csv("../../data/iris.csv")
+df = read_csv("../../data/iris.csv", index_col=False)
 x  = df.select_dtypes(exclude='object')
 
 pca   = PCA(n_components=1)
 x_pca = DataFrame(pca.fit_transform(X=x))
 print(pca.explained_variance_ratio_) # [0.92461872]
 
+x_pca.columns = ['pca']
 x_pca.to_csv("../../data/iris_pca.csv", index=False)
