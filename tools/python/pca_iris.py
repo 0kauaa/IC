241aa673bcdee2e3342fa706bae90509b08bc0d8
Kauã Santana <kauassilva0023@gmail.com>
2026-06-20 13:40:12 -0300

    aplica o pca com um compenente via python tools e salva em data.

---
+++ tools/python/pca_iris.py
@@ -0,0 +1,12 @@
+# aplica o pca, com 1 componente, ao iris e salva em iniciacao-cientifica/data/iris_pca.csv
+from pandas import read_csv, DataFrame
+from sklearn.decomposition import PCA 
+
+df = read_csv("../../data/iris.csv")
+x  = df.select_dtypes(exclude='object')
+
+pca   = PCA(n_components=1)
+x_pca = DataFrame(pca.fit_transform(X=x))
+print(pca.explained_variance_ratio_) # [0.92461872]
+
+x_pca.to_csv("../../data/iris_pca.csv", index=False)
